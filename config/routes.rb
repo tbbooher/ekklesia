67d5ac7dd82d94c8d9e96869d724263ad511b58e
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-23 15:44:42 -0500

    Make welcome#index root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#logout'
   get '/signup', to: 'users#new'
-
 
   resources :users
 
