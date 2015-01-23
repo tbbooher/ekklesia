cfef141a8e12251f7ab9bddd54167adaf4aacd5a
Brian Borge <brianborge@gmail.com>
2015-01-23 16:15:55 -0500

    Edit Politician Validations

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -5,5 +5,5 @@ class Politician < ActiveRecord::Base
   has_many :bills, through: :bill_votes
 
   validates_uniqueness_of :bioguide_id
-  validates_presence_of :bioguide_id, :first_name, :last_name, :party, :phone, :website, :office, :contact_form, :fax
+  validates_presence_of :bioguide_id, :first_name, :last_name
 end
