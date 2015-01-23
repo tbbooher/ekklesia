cfddd294870327c158b8fc6f887071e3f8bef0f0
Brian Borge <brianborge@gmail.com>
2015-01-23 17:14:56 -0500

    Create method to seed bills.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+require 'json'
+
+API = "https://congress.api.sunlightfoundation.com"
+API_KEY = ENV["CONGRESS_API"]
+bill_fields = "official_title,popular_title,short_title,summary,summary_short,urls,keywords"
+
+def bills(fields, page_count, page_number)
+  return JSON.parse(open(API + "/bills?apikey=#{API_KEY}&per_page=#{page_count}&page=#{page_number}&fields=" + fields).read)
+end
+
+
+
+number_of_bills = bills(fields, 50, 1)["count"]
+i = 1
+
+(number_of_bills / 50).times do
+  sleep(1)
+  bills(fields, 50, i)["results"].each do |b|
+    p Bill.create(
+      official_title: b["official_title"],
+      popular_title: b["popular_title"],
+      short_title: b["popular_title"],
+      summary: b["summary"],
+      summary_short: b["summary_short"],
+      congress_url: b["urls"]["congress"]
+    )
+  end
+  i+=1
+end
