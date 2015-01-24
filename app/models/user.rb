eeedc8f8290b6ab7ef599a9de97851896a976cac
HaileyR <hangyeolryu@gmail.com>
2015-01-23 19:11:36 -0800

    Reorder code for clarity.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :stances
   has_many :upvotes
-  has_secure_password
 
   validates_uniqueness_of :email, :username
   validates_presence_of :first_name, :last_name, :email
-
 end
