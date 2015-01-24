555349f98be8aed681514bebd2c16961daa0addd
Brian Borge <brianborge@gmail.com>
2015-01-24 16:12:26 -0500

    Remove bills seeding method.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,31 +4,7 @@ require 'json'
 
 API = "https://congress.api.sunlightfoundation.com"
 API_KEY = ENV["CONGRESS_API"]
-bill_fields = "official_title,popular_title,short_title,summary,summary_short,urls,keywords"
 
-def bills(fields, page_count, page_number)
-  return JSON.parse(open(API + "/bills?apikey=#{API_KEY}&per_page=#{page_count}&page=#{page_number}&fields=" + fields).read)
-end
-
-
-
-number_of_bills = bills(bill_fields, 50, 1)["count"]
-i = 1
-
-(number_of_bills / 50).times do
-  sleep(1)
-  bills(bill_fields, 50, i)["results"].each do |b|
-    p Bill.create(
-      official_title: b["official_title"],
-      popular_title: b["popular_title"],
-      short_title: b["popular_title"],
-      summary: b["summary"],
-      summary_short: b["summary_short"],
-      congress_url: b["urls"]["congress"]
-    )
-  end
-  i+=1
-end
 LEGISLATORS_OUT_OF_OFFICE = [11808,"false"]
 LEGISLATORS_IN_OFFICE = [540,"true"]
 LEGISLATOR_FIELDS = "bioguide_id,aliases,district,twitter_id,campaign_twitter_ids,youtube_id,facebook_id,terms"
