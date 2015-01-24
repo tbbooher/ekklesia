e1c453564ec6c40bda0dcd1ce012a2fe3062f8b1
Don J. Kahn <kahdojay@gmail.com>
2015-01-24 16:29:11 -0500

    Add votes#create action for upvotes.

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,2 +1,15 @@
 class UpvotesController < ApplicationController
+  def create
+    upvote = Upvote.new(upvote_params)
+    if upvote.save
+      redirect_to root_path
+    else
+      set_error('Login to upvote.')
+      redirect_to '/login'
+    end
+  end
+
+  def upvote_params
+    params.require(:upvote).permit(:user_id, :stance_id)
+  end
 end
