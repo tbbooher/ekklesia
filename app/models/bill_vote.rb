83dfc6364dcfa59e47d476407172d830f36a2a0a
Peter Jahnes <peter.jahnes@gmail.com>
2015-01-24 16:23:19 -0500

    Change politician to legislator

--- app/models/bill_vote.rb
+++ app/models/bill_vote.rb
@@ -1,7 +1,7 @@
 class BillVote < ActiveRecord::Base
   belongs_to :bill
-  belongs_to :politician
+  belongs_to :legislator
 
   validates_presence_of :bill_id
-  validates_presence_of :politician_id
+  validates_presence_of :legislator_id
 end
