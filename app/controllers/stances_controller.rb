6a354a34c3d0ebbf1d182e27c956bf07ba1e0e7f
HaileyR <hangyeolryu@gmail.com>
2015-01-24 09:55:51 -0800

    Add instance variables for a stance view after deleting unused code.

--- app/controllers/stances_controller.rb
+++ app/controllers/stances_controller.rb
@@ -13,45 +13,24 @@ class StancesController < ApplicationController
   end
 
   def show
-    @stance = Stance.find(param[:id])
+    @stance = Stance.find(params[:id])
+    @position = Position.find(@stance.position_id)
+    @author = User.find(@stance.user_id)
   end
 
   def create
-      @question = Question.new(question_params)
-      if @question.save
-        @question.create_tags(params[:question][:question_tags]) if params[:question][:question_tags]
-        redirect_to question_path(@question)
+    p params
+      @stance = current_user.stances.new(stances_params)
+      if @stance.save
+        redirect_to stance_path(@stance)
       else
-        flash[:alert] = "ERROR: #{@question.errors.full_messages.join("; ")}"
+        flash[:alert] = "ERROR: #{@stances.errors.full_messages.join("; ")}"
         render :new
       end
     end
 
-    def edit
-        @question = Question.find(params[:id])
-        redirect_to root_path unless @question.user_id == current_user.id
-      end
-
-      def update
-        @question = Question.find(params[:id])
-        redirect_to root_path unless @question.user_id == current_user.id
-        if @question.update(question_params)
-          redirect_to @question
-        else
-          flash[:alert] = "ERROR: #{@question.errors.full_messages.join("; ")}"
-          render :edit
-        end
-      end
-
-      def destroy
-        @question = Question.find(params[:id])
-        redirect_to root_path unless @question.user_id == current_user.id || current_user.is_admin
-        @question.destroy
-        redirect_to root_path
-      end
-
-      private
-        def question_params
-          params.require(:question).permit(:position_id)
-        end
+  private
+    def stances_params
+      params.require(:stance).permit(:position_id)
+    end
 end
