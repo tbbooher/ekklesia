3f9230db649c5d01c9667e95c9f9a8b77b325345
Don J. Kahn <kahdojay@gmail.com>
2015-01-24 16:25:03 -0500

    Write spec tests for upvotes.

---
+++ spec/controllers/votes_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe UpvotesController do
+  context 'upvote creation' do
+    before :each do
+      @user = FactoryGirl.create(:user)
+      @stance = @user.stances.create(attributes_for(:stance))
+      @upvote = @stance.upvotes.create(attributes_for(:upvote))
+    end
+
+    it 'creates an upvote record with the correct user_id' do
+      expect(@upvote.user_id).to eq(@user.id)
+    end
+
+    it 'creates an upvote record with the correct stance_id' do
+      expect(@upvote.stance_id).to eq(@stance.id)
+    end
+
+  end
+
+  context 'saves to db' do
+    it 'adds an upvote to the upvotes table in the db' do
+      expect{ create(:upvote) }.to change{ Upvote.count }.by(1)
+    end
+  end
+end
