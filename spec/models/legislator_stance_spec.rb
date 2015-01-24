b53c104adc60514cf1cee29b57d088da0a287b2b
Peter Jahnes <peter.jahnes@gmail.com>
2015-01-24 16:04:51 -0500

    Change model name to legialator

--- spec/models/legislator_stance_spec.rb
+++ spec/models/legislator_stance_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-describe PoliticianStance do
+describe LegislatorStance do
   it 'has a valid factory' do
-    expect(build(:politician_stance)).to be_valid
+    expect(build(:legislator_stance)).to be_valid
   end
 
   it "is invalid without a politician" do
-    expect(build(:politician_stance, politician_id: nil)).to be_invalid
+    expect(build(:legislator_stance, legislator_id: nil)).to be_invalid
   end
 
   it "is invalid without a stance" do
-    expect(build(:politician_stance, stance_id: nil)).to be_invalid
+    expect(build(:legislator_stance, stance_id: nil)).to be_invalid
   end
 
 end
