b8c1029adf699b60f59fd1cb16878f107b89f4dc
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-25 17:35:51 -0500

    Add state field for User strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :middle_name, :username, :city, :about, :email,  :password, :is_admin)
+      params.require(:user).permit(:first_name, :last_name, :middle_name, :username, :city, :state, :about, :email,  :password, :is_admin)
     end
 
     def admin_param_fix
