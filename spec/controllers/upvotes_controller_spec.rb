7814320ffc7507df7fbff9077aff715bfd5c6017
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-25 16:19:49 -0500

    Clean up upvote spec.

--- spec/controllers/upvotes_controller_spec.rb
+++ spec/controllers/upvotes_controller_spec.rb
@@ -1,13 +1,7 @@
 require 'rails_helper'
 
 describe UpvotesController do
-  # it "creates an upvote in the db" do
-  #   expect{
-  #     post :create, upvote: attributes_for(:upvote)
-  #     }.to change(Upvote, :count).by(1)
-  # end
-
-  it "creates a new Answer" do
+  it "creates an upvote in the db" do
     user = User.create(first_name: 'first', last_name: 'last', username: 'username', password: 'a', email: 'email@example.com')
     stance = Stance.create(user_id: 1, position_id: 1)
     expect {
