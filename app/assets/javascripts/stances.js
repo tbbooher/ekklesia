fca77a0f443dd1fa3ef865f52bcf174d27108449
HaileyR <hangyeolryu@gmail.com>
2015-01-27 11:19:57 -0800

    Add event listener functions for the stance donation page.

--- app/assets/javascripts/stances.js
+++ app/assets/javascripts/stances.js
@@ -1,4 +1,24 @@
-$('.side-nav').on("click", "a", function(event){
+$('.side-nav').on("click", "a", updateStanceView);
+$('#donation_amount').on('keyup', distribute)
+$('.each_amount').on('keyup', updateTotal)
+
+var distribute = function(event){
+    var totalAmount = $(event.target).val()
+    var amount = totalAmount / $('.each_amount').length
+    $('.each_amount').val(amount)
+};
+
+var updateTotal = function(event){
+    var total = 0
+    $('.each_amount').each(function(){
+        if(parseInt($(this).val()) > 0){
+            total += parseInt($(this).val())
+        }
+    })
+    $('#donation_amount').val(total)
+};
+
+var updateStanceView = function(event){
   event.preventDefault();
   $target = $(event.target);
   $.ajax({
@@ -8,4 +28,4 @@ $('.side-nav').on("click", "a", function(event){
   }).done(function(response){
     $('.stances').replaceWith(response);
   });
-});
+};
