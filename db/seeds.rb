eee6bc4dca584fef68c0e0de940f1ddf6db51af8
Brian Borge <brianborge@gmail.com>
2015-01-27 14:27:22 -0500

    Fix bug in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 include SeedHelper
-User.create(first_name: 'a', last_name: 'a', last_name: 'a@a.com', username: 'a', password_digest: 'a')
-# seed real legislators
-SeedHelper::LegislatorSeed::insert_basic_legislators
-SeedHelper::LegislatorSeed::insert_details
-SeedHelper::LegislatorSeed::insert_image_url
-SeedHelper::BioScraper::get_all_biographies
+# User.create!(first_name: 'a', last_name: 'a', last_name: 'a@a.com', username: 'a', password_digest: 'a')
+# # seed real legislators
+# SeedHelper::LegislatorSeed::insert_basic_legislators
+# SeedHelper::LegislatorSeed::insert_details
+# SeedHelper::LegislatorSeed::insert_image_url
+# SeedHelper::BioScraper::get_all_biographies
 
 # seed fake legislators
-# 100.times do
-#   Legislator.create(bioguide_id: Faker::Company.ein, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-# end
+100.times do
+  Legislator.create(bioguide_id: Faker::Company.ein, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
 
 # seed users
-50.times do
-  User.create(first_name: Faker::Name.name, last_name: Faker::Name.name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, password:'a', about: Faker::Company.catch_phrase, city: Faker::Address.city, state: Faker::Address.state)
+5.times do
+  User.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, password:'a', about: Faker::Company.catch_phrase, city: Faker::Address.city, state: Faker::Address.state)
 end
 
 # define universe of issues and positions
@@ -28,28 +28,30 @@ issue_positions = {
 
 # seed issues and positions
 issue_positions.each do |issue, position_array|
-  new_issue = Issue.create(description: issue)
-  position_array.each { |position| new_issue.positions.create(description: position) }
+  new_issue = Issue.create!(description: issue)
+  position_array.each { |position| new_issue.positions.create!(description: position) }
 end
 
 # seed stances, upvotes
 User.all.each do |user|
-  50.times { user.stances.create(position_id: rand(Position.count) + 1) }
+  50.times { user.stances.create!(position_id: rand(Position.count) + 1) }
 end
 
 Stance.all.each do |stance|
   rand(50).times do
-    stance.legislator_stances.create(legislator_id: (1 + rand(Legislator.count)))
-    stance.upvotes.create(user_id: (1 + rand(User.count)))
+    stance.legislator_stances.create!(legislator_id: (1 + rand(Legislator.count)))
+  end
+  (1..rand(User.count)).each do |i|
+    stance.upvotes.create!(user_id: i)
   end
 end
 
 # seed donations
 User.all.each do |user|
-  user.donations.create(legislator_id: (1 + rand(Legislator.count)), stance_id: (1 + rand(Stance.count)), amount: rand(1000000))
+  user.donations.create!(legislator_id: (1 + rand(Legislator.count)), stance_id: (1 + rand(Stance.count)), amount: rand(1000000))
 end
 
 # seed legislator_issues
 100.times do
-  LegislatorIssue.create(issue_id: (1 + rand(Issue.count)),legislator_id: (1 + rand(Legislator.count)), issue_score: 0.5)
+  LegislatorIssue.create!(issue_id: (1 + rand(Issue.count)),legislator_id: (1 + rand(Legislator.count)), issue_score: 0.5)
 end
