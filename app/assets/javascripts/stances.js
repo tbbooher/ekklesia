bd39b7ee55159b70a12e55fd26138c6b59e2f089
HaileyR <hangyeolryu@gmail.com>
2015-01-27 12:34:51 -0800

    Change element id.

--- app/assets/javascripts/stances.js
+++ app/assets/javascripts/stances.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
   $('.side-nav').on("click", "a", updateStanceView);
-  $('#donation_amount').on('keyup', distribute)
+  $('#total_amount').on('keyup', distribute)
   $('.each_amount').on('keyup', updateTotal)
 })
 
 var distribute = function(event){
-    var totalAmount = $(event.target).val()
-    var amount = totalAmount / $('.each_amount').length
-    $('.each_amount').val(amount)
+  var totalAmount = $(event.target).val()
+  var amount = totalAmount / $('.each_amount').length
+  $('.each_amount').val(amount.toFixed(2))
 };
 
 var updateTotal = function(event){
-    var total = 0
-    $('.each_amount').each(function(){
-        if(parseInt($(this).val()) > 0){
-            total += parseInt($(this).val())
-        }
-    })
-    $('#donation_amount').val(total)
+  var total = 0
+  $('.each_amount').each(function(){
+      if(parseInt($(this).val()) > 0){
+          total += parseInt($(this).val())
+      }
+  })
+  $('#total_amount').val(total.toFixed(2))
 };
 
 var updateStanceView = function(event){
