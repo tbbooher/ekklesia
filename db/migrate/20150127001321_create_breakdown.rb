c0ee9280a1f6dbf2a68f9ff916182fe636fde661
Brian Borge <brianborge@gmail.com>
2015-01-26 20:02:20 -0500

    Edit spelling in breakdown table.

--- db/migrate/20150127001321_create_breakdown.rb
+++ db/migrate/20150127001321_create_breakdown.rb
@@ -1,10 +1,10 @@
 class CreateBreakdown < ActiveRecord::Migration
   def change
     create_table :breakdowns do |t|
-      t.interger :r_yea
-      t.interger :r_nay
-      t.interger :d_yea
-      t.interger :d_nay
+      t.integer :r_yea
+      t.integer :r_nay
+      t.integer :d_yea
+      t.integer :d_nay
       t.references :bill
     end
   end
