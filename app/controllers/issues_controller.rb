ac8ca2d2593ee5ed848706ec7a5c6dc0a30dda9b
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-28 17:16:01 -0500

    Change index to show route in Issue Controller.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,7 @@
 class IssuesController < ApplicationController
-  def index
-    @legislators = Legislator.all
-    @issues = Issue.all
+  def show
+    @issue = Issue.find(params[:id])
+    @stances = Stance.where(issue_id: @issue.id)
+    @legislators = Legislator.filter_legislators_by_vote_count(@issue.id)
   end
 end
