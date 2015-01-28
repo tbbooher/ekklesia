bea5e1cc7598acb51441767ba90f986af99d5729
Brian Borge <brianborge@gmail.com>
2015-01-28 14:54:24 -0500

    Describe donation unit tests.

---
+++ spec/models/donation_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Donation do
+  it "creates a new records in donation table"
+  it "is valid with a stance, legislator and amount"
+  it "is invalid without a stance"
+  it "is invalid without a legislator"
+end
