ad946acf41177867678257d8b6a1e12f2302640b
Don J <kahdojay@gmail.com>
2015-01-28 14:57:47 -0500

    Add welcome link, page, route, controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,9 @@ class WelcomeController < ApplicationController
     @issues = Issue.fetch(4)
     render layout: 'welcome'
   end
+
+  def about
+    @issues = Issue.fetch(4)
+    render layout: 'application'
+  end
 end
