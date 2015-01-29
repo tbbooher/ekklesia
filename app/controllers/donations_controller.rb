527d47c90a40c3130a9b01dd2aed7a1951631a2a
Don J <kahdojay@gmail.com>
2015-01-29 00:58:06 -0500

    Protect some donations/stances routes to check for logged_in?

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,12 +2,14 @@ class DonationsController < ApplicationController
   include AuthsHelper
 
   def show
+    redirect_to :login unless logged_in?
     @stance = Stance.find(params[:stance_id])
     @legislators = @stance.legislators
     @legislator_ids = @stance.legislators.map{|l|l.id}
   end
 
   def create
+    redirect_to :login unless logged_in?
     amount = params[:donation]["amount"].to_i * 100
     token = params[:stripeToken]
 
