47bc2ffd2d13e6c852c698c2108b2f11194d74ec
HaileyR <hangyeolryu@gmail.com>
2015-01-29 20:22:59 -0800

    Uncomment donations.js file.

--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -1,24 +1,16 @@
-// $(document).ready(function(){
-//   $('.donations').on("click", getUserDonations);
-//   $('.stances').on("click", getUserStances);
-// });
+$(document).ready(function(){
+  $('.donations').on("click", getUserDonations);
+  $('.stances').on("click", getUserStances);
+});
 
-// function getUserDonations(event) {
-//   event.preventDefault();
-//   console.log("donations");
-//   var $target = $(event.target);
-//   debugger
-//   $.ajax({
-//     url: $target.attr("href"),
-//     type: "GET"
-//   }).done(function(resonse){
-//     console.log(resonse);
-//     $('.user-table').html(resonse);
-//   });
-// }
-
-// function getUserStances(event) {
-//   event.preventDefault();
-//   console.log("stances");
-//   $.ajax({}).done(function(){});
-// }
+function getUserDonations(event) {
+  event.preventDefault();
+  var $target = $(event.target);
+  $.ajax({
+    url: $target.attr("href"),
+    type: "GET"
+  }).done(function(resonse){
+    console.log(resonse);
+    $('.user-table').html(resonse);
+  });
+}
