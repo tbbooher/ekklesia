0278f3892d0087b49e2f9cee04106137bd9a3ed3
Peter Jahnes <peter.jahnes@gmail.com>
2015-02-05 22:42:58 -0500

    Disable the donate button.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -10,25 +10,11 @@ class DonationsController < ApplicationController
   end
 
   def create
-    redirect_to :login unless logged_in?
-    amount = params[:donation]["amount"].to_i * 100
-    token = params[:stripeToken]
-
-    @charge = Stripe::Charge.create(
-      :amount => amount,
-      :currency => "usd",
-      :card => token,
-      :description => "something about your customer"
-    )
-
-    Donation.create_records(params)
+    redirect_to :root
   end
 
   def new
-    redirect_to :login unless logged_in?
-    @stance = Stance.find(params[:stance_id])
-    @legislators = @stance.legislators
-    @legislator_ids = @stance.legislators.map{|l|l.id}
+    redirect_to :root
   end
 
   private
