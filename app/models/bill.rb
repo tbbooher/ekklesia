2a6246d16fef69753d4be72a51ce7bb4ae0aa4dd
Don J <kahdojay@gmail.com>
2015-02-09 15:09:39 -0500

    Refactor bill model.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,8 +1,8 @@
 class Bill < ActiveRecord::Base
-  has_many :bill_votes
-  has_many :breakdowns
-  has_many :legislators, through: :bill_votes
   belongs_to :issue
+  has_many :bill_votes
+  has_many :legislators, through: :bill_votes
+  has_many :breakdowns
 
   validates_presence_of :official_title
 end
