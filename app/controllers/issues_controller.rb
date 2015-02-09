8b2b6d9ae04f09093fbde3d8e440e63f71828cae
Don J <kahdojay@gmail.com>
2015-02-09 15:38:01 -0500

    Remove issues controller.

--- app/controllers/issues_controller.rb
+++
@@ -1,6 +0,0 @@
-class IssuesController < ApplicationController
-  def show
-    @issue = Issue.find(params[:id])
-    @legislators = Legislator.filter_legislators_by_vote_count(@issue.id)
-  end
-end
