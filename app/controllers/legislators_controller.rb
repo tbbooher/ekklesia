42a599937b5872499c984fd8f0cc9f536e23608d
Don J <kahdojay@gmail.com>
2015-02-09 15:38:34 -0500

    Refactor legislators controller.

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -2,6 +2,7 @@ class LegislatorsController < ApplicationController
   def index
     @legislators = Legislator.all.order(:last_name, first_name: :desc)
   end
+
 	def show
 		@legislator = Legislator.find(params[:id])
 	end
