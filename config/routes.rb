8d054efa8be8095e0646f1ade0cc05c962cdf152
Brian Borge <brianborge@gmail.com>
2015-02-18 20:28:32 -0500

    Create route for donations email.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :stances do
     get '/donations/new', to: 'donations#new'
     post '/donations', to: 'donations#create'
+    post '/donations/donationemail', to: 'donations#email'
   end
 
   resources :donations, only: [:show]
