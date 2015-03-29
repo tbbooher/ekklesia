73666c7a47c261d32ff6a5a8204fe18745a3e78a
Don J <kahdojay@gmail.com>
2015-03-29 01:20:32 -0400

    Error handling for votes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
   include AuthsHelper
+  include ErrorsHelper
 
   def create
     # [f, s] = Vote.calc_crazy_fs
-    Vote.create(user_id: current_user.id, bill_id: params[:vote][:bill_id].to_i, direction: params[:commit])
+    if logged_in?
+      Vote.create(user_id: current_user.id, bill_id: params[:vote][:bill_id].to_i, direction: params[:commit])
+    else
+      set_error("Login to vote.")
+    end
     redirect_to :root
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   def index
     @bills = logged_in? ? Bill.not_voted_on(current_user) : Bill.all
     @bill = @bills[0]
+    @voted_bills = current_user.bills if logged_in?
     @vote = Vote.new(bill: @bill)
     render layout: 'welcome'
   end
