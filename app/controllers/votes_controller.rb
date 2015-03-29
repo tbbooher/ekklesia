ac1b6748fd9a5a5c675e4231009068b01af68760
Don J <kahdojay@gmail.com>
2015-03-29 01:40:17 -0400

    Rescore bill upon votes_controller#create.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,11 @@ class VotesController < ApplicationController
   include ErrorsHelper
 
   def create
-    # [f, s] = Vote.calc_crazy_fs
     if logged_in?
-      Vote.create(user_id: current_user.id, bill_id: params[:vote][:bill_id].to_i, direction: params[:commit])
+      @bill = Bill.find(params[:vote][:bill_id].to_i)
+      Vote.create(user_id: current_user.id, bill_id: @bill.id, direction: params[:commit])
+      @bill.rescore
+
     else
       set_error("Login to vote.")
     end
