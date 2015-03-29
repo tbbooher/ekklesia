c53a2584f5761c92d7af7fd9c0726aa0717bc0df
Don J <kahdojay@gmail.com>
2015-03-29 00:12:43 -0400

    Fix seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # seed users
 5.times do
-  User.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, password:'a', about: Faker::Company.catch_phrase, city: Faker::Address.city, state: Faker::Address.state, fiscal_mean: rand, social_mean: rand)
+  User.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, password:'a', about: Faker::Company.catch_phrase, city: Faker::Address.city, state: Faker::Address.state)
 end
